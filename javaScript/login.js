c5a0d78ca392352c256e4b314e5ba917ca9e5ec8
Shalin <nirvan.work@gmail.com>
2022-12-03 12:00:23 +0530

    removed javascript

--- javaScript/login.js
+++
@@ -1,7 +0,0 @@
-// const usernameTag = document.querySelector("h3");
-// const usernameInput = document.querySelector('input[type="text"]');
-
-// usernameTag.style.color = "red";
-// usernameTag.innerText = "Incorrect username";
-
-// usernameInput.style.border = "1px solid red";
